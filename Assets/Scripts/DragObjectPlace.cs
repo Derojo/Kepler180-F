b33bf1bcc825a5b1365952b7ed28fa8fec41f246
Derojo <bakker.rowin@gmail.com>
2016-12-07 14:18:49 +0100

    Added grid functionality Customizable grid size Snapping

---
+++ Assets/Scripts/DragObjectPlace.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObjectPlace : MonoBehaviour
+{
+    public GameObject objectInDrag;
+    public Vector3 offset;
+    //public PlaceSelect placeSelect;
+
+    private PlaceBehaviour lastPlaceSelected;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfor;
+
+        if (Physics.Raycast(ray, out hitInfor))
+        {
+            Debug.Log(hitInfor.collider.gameObject);
+            objectInDrag.SetActive(true);
+
+            Vector3 pos = new Vector3(hitInfor.collider.transform.position.x, offset.y, hitInfor.collider.transform.position.z);
+            objectInDrag.transform.position = pos;
+        }
+        else
+        {
+            objectInDrag.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Static container class for all grid cells on the map.
+ * The grid always starts with x-position and z-position values on 0.
+ */
+static class Grid
+{
+    /**
+     * Two-dimensional array containing instances of the grid cell for faster access. 
+     */
+    private static GameObject[,] grid;
+     
+     /**
+      * Maximum grid width.
+      */
+     public static int xMax = 10;
+
+    /**
+     * Maximum grid depth.
+     */
+    public static int zMax = 10;
+     
+     /**
+      * Initializes the container array
+      */
+     public static void initializeArray(int xMax, int zMax) {
+         grid = new GameObject[xMax, zMax];
+     }
+
+/**
+ * Returns a grid cell at given position. Faster than iterating through a one-dimensional array with comparisons.
+ */
+public static GameObject getGridCellAtPosition(int x, int z) {
+         return grid[x, z];
+     }
+     
+     /**
+      * Adds a grid cell to the container.
+      */
+     public static void addCell(int x, int z, GameObject cell) {
+         grid[x, z] = cell;
+     }
+     
+     public static GameObject[,] getGrid() {
+         return grid;
+     }
+ }
