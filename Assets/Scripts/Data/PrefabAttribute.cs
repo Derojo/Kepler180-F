a78b75f4a4c704af7e9dce867e5a326113e964e2
Goizz <gijstempel@msn.com>
2016-12-07 13:36:30 +0100

    Added button for next turn. This is handled by the turnmanager

---
+++ Assets/Scripts/Data/PrefabAttribute.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = true)]
+public class PrefabAttribute : Attribute
+{
+    public readonly string Name;
+    public readonly bool Persistent;
+    public readonly string Parent;
+
+    public PrefabAttribute(string name, bool persistent, string parent)
+    {
+        Name = name;
+        Persistent = persistent;
+        Parent = parent;
+    }
+
+    public PrefabAttribute(string name)
+    {
+        Name = name;
+        Persistent = false;
+    }
+}
