169934b1920a7fe80562e5e8353041e83f00dd33
Derojo <bakker.rowin@gmail.com>
2016-12-20 00:11:47 +0100

    Added better lighting settings Added light probes for better GI light simulation changed colorgenerator prefab - Add lighting to generator when its on - Added particle beam when its on - Removed unnecessary files

--- Assets/Scripts/Buildings/ColorGenerator.cs
+++ Assets/Scripts/Buildings/ColorGenerator.cs
@@ -21,8 +21,10 @@ public class ColorGenerator : MonoBehaviour
     private Text buildTimeInfo;
     public bool generatePower = false;
     // Use this for initialization
+
     void Start()
     {
+
         ColorGeneratorCode = (int)selectedColor;
         ColorGeneratorName = selectedColor.ToString();
         if (!blueprint) {
@@ -69,7 +71,10 @@ public class ColorGenerator : MonoBehaviour
             buildingInfoCanvas.transform.GetChild(1).gameObject.SetActive(false);
             buildingInfoCanvas.transform.GetChild(2).gameObject.SetActive(true);
             turnedOn = true;
+
             buildingKrachtInfo.SetActive(true);
+            gameObject.transform.GetChild(3).gameObject.SetActive(true);
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
             return;
         }
         if (buildingDone && turnedOn) {
