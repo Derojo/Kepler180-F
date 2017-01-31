78d9d1d3f53d8fc5f3dca270dc9efe4e115fb992
Derojo <bakker.rowin@gmail.com>
2017-01-31 11:58:29 +0100

    - Changes in mainstation script (!) Need to set rangeX and rangeZ in inspector to 3

--- Assets/Scripts/Buildings/MainStation.cs
+++ Assets/Scripts/Buildings/MainStation.cs
@@ -8,9 +8,11 @@ public class MainStation : MonoBehaviour {
     public GridManager grid;
     public float offset;
 
+    public int rangeX;
+    public int rangeZ;
     public int range;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         StartCoroutine(waitForPlacement(1));
     }
     
@@ -25,10 +27,10 @@ public class MainStation : MonoBehaviour {
         station.transform.parent = firstcell.transform;
         Vector3 pos = new Vector3(firstcell.transform.position.x, (station.transform.position.y+offset), firstcell.transform.position.z);
         firstcell.GetComponent<Tile>().currentObject = station;
-        firstcell.GetComponent<Tile>().tileType = (int)this.GetComponent<BuildingType>().type;
+        firstcell.GetComponent<Tile>().tileType = (int)Types.buildingtypes.maingenerator;
         station.transform.position = pos;
         station.SetActive(true);
-        grid.updateRange(startX, startZ, range);
+        grid.updateRange(startX, startZ, rangeX, rangeZ);
     }
 
     public IEnumerator waitForPlacement(float time) {
